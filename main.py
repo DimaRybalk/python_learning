d5b1ac6e02f1d9261af33f36df92f7fc69c240c0
unknown <dima.rubaka1@gmail.com>
2026-04-03 03:22:42 +0200

    new name

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from routers import AuthenticationRouter, GenreRouter, MovieRouter, UserRouter
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Lifeflix")
+app = FastAPI(title="LifeFlix")
 
 app.include_router(AuthenticationRouter.router)
 app.include_router(UserRouter.router)
